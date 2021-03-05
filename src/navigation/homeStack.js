0bbc2f74abefacaabb6b8946a4051c320675c6ff
Thomas Allen <gtallenpadi13@gmail.com>
2021-03-05 16:25:30 -0600

    Reemplazando boton de perfil por boton de regresar

--- src/navigation/homeStack.js
+++ src/navigation/homeStack.js
@@ -18,15 +18,12 @@ const screens = {
     login:{
         screen: LoginPage,
         /* Barra superior */
-        // navigationOptions:{
-        //     headerTitle:()=>(<Text style={styles.headerTitle}>Login</Text>),
-        //     headerRight: () =>(
-        //         <ButtonText text="Logout" color="#000" fontSize={15} callback={()=>console.log("Logout")}/>
-        //     ),
-        //     headerLeft: ()=>(
-        //         <HeaderButton icon="user-circle" callback={()=>console.log("My profile")}/>
-        //     )
-        // },
+        navigationOptions:{
+            headerTitle:()=>(<Text style={styles.headerTitle}>Login</Text>),
+            headerRight: () =>(
+                <ButtonText text="Logout" color="#000" fontSize={15} callback={()=>console.log("Logout")}/>
+            ),
+        },
     },
     signUp: {
         screen:SignUpPage
