6b94a7299259c88444c5ccfd4557b4727856be99
Thomas Allen <gtallenpadi13@gmail.com>
2021-03-06 17:47:24 -0600

    Creando estructura de la pantalla

---
+++ src/screens/MyProjects.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native'
+import {searchBar} from '../components/searchBar'
+
+const MyProjects = () =>{
+    return(
+        <View>
+
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#ecedeb',
+    }
+})
