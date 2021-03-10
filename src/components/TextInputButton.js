6c88e36a1107cff157bc4f81234e74e399fb67c3
Thomas Allen <gtallenpadi13@gmail.com>
2021-03-10 06:06:03 -0600

    Creando nuevo componente para inputs en MyProfile

--- src/components/TextInputButton.js
+++ src/components/TextInputButton.js
@@ -36,6 +36,36 @@ const ButtonIcon = (
     )
 }
 
+const ToggleTextInput = (
+    {
+        onChangeText,
+        value,
+        onBlur,
+    }) => {
+        const [editable, setEditable] = useState(false);
+    return (
+        <View style={styles.styleButtonPassword}> 
+            <TextInput 
+            editable={editable}
+            style = {styles.inputPassword}  
+            secureTextEntry={secureText} 
+            onChangeText={onChangeText} 
+            value={value}
+            onBlur={onBlur}/>
+            <TouchableOpacity style={styles.passwordInput}>
+                <Icon style = {styles.icon} containerStyle={{marginRight:10 , backgroundColor:'white'}}
+                    name='edit'
+                    type='font-awesome'
+                    color='black'
+                    onPress={()=>{editable ? setEditable(true):setEditable(false)}}
+                   
+            />
+            </TouchableOpacity>
+        </View>
+       
+    )
+}
+
 
 
 
@@ -59,4 +89,4 @@ const styles = StyleSheet.create({
  
 })
 
-export {ButtonIcon};
+export {ButtonIcon, ToggleTextInput};
