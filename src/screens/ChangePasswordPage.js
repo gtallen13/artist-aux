4757f7fb7f9f0bb60979fb62b72285b47f693b46
JonathanA03 <jonathanaleman292@gmail.com>
2021-03-10 03:39:27 -0600

    Agregando funcion de cambiar contrasena

--- src/screens/ChangePasswordPage.js
+++ src/screens/ChangePasswordPage.js
@@ -1,32 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {StyleSheet, Text, View} from 'react-native'
 import {Icon} from 'react-native-elements'
 import {ButtonIcon} from '../components/TextInputButton'
 import {ButtonLogin} from '../components/Button'
-
+import {firebase} from '../firebase';
 
 const ChangePasswordPage = ({navigation}) => {
-   return(
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
 
+    const handleReauthenticate = async (currentPassword) => {
+        let user = firebase.auth().currentUser;
+        let cred = firebase.auth().EmailAuthProvider.credentials(user.email, currentPassword);  
+        return user.reauthenticateWithCredential(cred)
+    }
+
+    const handleChangePassword = async () => {
+
+        handleReauthenticate(currentPassword).then(() => {
+            let user = firebase.auth().currentUser;
+            user.updatePassword(newPassword).then(() => {
+            console.log('Password was changed');
+            }).catch ((error) => {
+                console.log(error.message);
+            });
+
+        }).catch((error) => {
+            console.log(error.message)
+        });
+    }
+
+
+
+   return(
     <View style={styles.container}>
         <View style={styles.inputText}> 
             <Text style={styles.email}>Old Password</Text>
-                <ButtonIcon callback={()=> console.log("Press")} iconName='edit' placeholderName='Password'/>
+                <ButtonIcon  
+                callback={()=> console.log("Press")} 
+                iconName='edit' 
+                placeholderName='Password'
+                onChangeText={setCurrentPassword}
+                value={currentPassword}
+                />
         
             <Text style={styles.password}>New Password</Text>
-                <ButtonIcon callback={()=> console.log("Press")} iconName='edit' placeholderName='Password'/>
+                <ButtonIcon 
+                callback={()=> console.log("Press")} iconName='edit' 
+                placeholderName='Password'
+                onChangeText={setNewPassword}
+                value={newPassword}
+                />
             
             <Text style={styles.password}>Confirm New Password</Text>
-                <ButtonIcon callback={()=> console.log("Press")} iconName='edit' placeholderName='Password'/>
+                <ButtonIcon 
+                callback={()=> console.log("Press")} iconName='edit' 
+                placeholderName='Password'/>
         </View>
         <View>
-            <ButtonLogin text={"Change password"}/>
+            <ButtonLogin 
+            callback={handleChangePassword}
+            text={"Change password"}
+            
+            />
         </View>
     </View>   
    )
-   
-    
-
 }
 
 const styles = StyleSheet.create({
