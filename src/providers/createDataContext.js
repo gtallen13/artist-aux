986e9d26229947008aee95b6a3a467381f3ecf4d
Thomas Allen <gtallenpadi13@gmail.com>
2021-03-26 12:29:22 -0600

    Creando plantilla parar contextos

---
+++ src/providers/createDataContext.js
@@ -0,0 +1,19 @@
+import React, {useReducer,createContext} from 'react';
+
+export default (reducer, actions, defaultvalue) =>{
+    const Context = createContext();
+    const Provider = ({children}) =>{
+        const [state,dispatch] = useReducer(reducer,defaultvalue);
+
+        const boundActions={};
+        for (let key in actions){
+            boundActions[key]=actions[key](dispatch);
+        }
+        return (
+            <Context.Provider value={{state,...boundActions}}>
+                {children}
+            </Context.Provider>
+        )
+        return {Context, Provider}
+    }
+}
