956eb87a64da54987b2d18fa37c5d49435d7085d
Thomas Allen <gtallenpadi13@gmail.com>
2021-03-27 08:52:05 -0600

    Agregando AuthProvider en App.js

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Navigator from './src/navigation/homeStack'
+import {Provider as AuthProvider} from './src/providers/AuthContext'
 export default function App() {
   return (
-    <Navigator/>
+    <AuthProvider>
+      <Navigator/>
+    </AuthProvider>
   );
 }
