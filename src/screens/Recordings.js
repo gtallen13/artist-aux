439592dd46c9ac0a3603aecdd3d35b0775d708d7
Thomas Allen <gtallenpadi13@gmail.com>
2021-04-05 12:08:34 -0600

    Reproduccion de audio: funciona

--- src/screens/Recordings.js
+++ src/screens/Recordings.js
@@ -1,99 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import {View, TextInput, ScrollView,StyleSheet, Text, useColorScheme} from 'react-native'
+import {View,  ScrollView,StyleSheet, Text} from 'react-native'
 import {Icon} from 'react-native-elements'
 import { ButtonStopNote } from '../components/Button'
 import { TextNote } from '../components/ButtonText'
-
-//https://dzone.com/articles/react-native-record-audio-tutorial
-import AudioRecorderPlayer, {
- AVEncoderAudioQualityIOSType,
- AVEncodingOption,
- AudioEncoderAndroidType,
- AudioSet,
- AudioSourceAndroidType,
-} from 'react-native-audio-recorder-player'
-import { Platform } from 'react-native'
+import {Audio} from 'expo-av'
 const Recordings = ({navigation}) =>{
     const [isRecording, setIsRecording] = useState(false)
     const [isPlaying, setIsPlaying] = useState(false)
-    const [recordingdSecs, setRecordingSecs] = useState(0);
-    const [recordingTime, setRecordingTime] = useState('00:00:00');
-    const [currentPositionSec, setCurrentPositionSec] = useState(0);
-    const [currentDurationSec, setCurrentDurationSec] = useState(0);
-    const [playtime, setPlaytime] = useState('00:00:00')
-    const [duration, setDuration] = useState('00:00:00')
-    
+    const  [sound, setSound] = useState();
 
-    const audioRecorderPlayer = new AudioRecorderPlayer();
-    audioRecorderPlayer.setSubscriptionDuration()
-
-    const onStartRecord = async ()=>{
-        setIsRecording(true)
-        console.log ('recording')
-        const path = Platform.select({
-            ios:'hello.m4a',
-            android: 'Music/hello.m4a', 
-        })
-        const audioSet = {
-            AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
-            AudioSourceAndroid: AudioSourceAndroidType.MIC,
-            AVEncoderAudioQualityIOS: AVEncoderAudioQualityIOSType.high,
-            AVNumberofChannelsKeyIOS:2,
-            AVFormatIDKeyIOS: AVEncodingOption.aac,
-        }
-        console.log('audioSet',audioSet);
-        const uri = await audioRecorderPlayer.startRecorder(path, audioSet);
-        
-        audioRecorderPlayer.addPlayBackListener((e) => {
-            setRecordingSecs(e.current_position)
-            setRecordingTime(audioRecorderPlayer.mmssss(Math.floor(e.current_position)))
-        })
-        console.log(uri)
-        console.log(`uri: ${uri}`);
+    const playSound = async()=>{
+        console.log('Loading Sound');
+        const {sound} = await Audio.Sound.createAsync(
+            require('../../assets/jeff.mp3')
+        )
+        setSound(sound)
+        console.log('Playing Sound')
+        await sound.playAsync();
     }
-    const onStopRecord = async ()=>{
-        setIsRecording(false)
-        console.log('not recording');
-        const result = await audioRecorderPlayer.stopRecorder();
-        audioRecorderPlayer.removePlayBackListener();
-        setRecordingSecs(0)
-        console.log(result);
-    }
-
-    const onStartPlay = async (e)=>{
-        setIsPlaying(true)
-        console.log('onStartPlay')
-        const path = 'hello.m4a'
-        const msg = await audioRecorderPlayer.startPlayer(path);
-        audioRecorderPlayer.setVolume(1.0)
-        console.log(msg)
-        audioRecorderPlayer.addPlayBackListener((e)=>{
-            if (e.current_position === e.duration)
-            {
-                console.log('finished');
-                audioRecorderPlayer.stopPlayer();
-            }
-        })
-        setCurrentPositionSec(e.current_position);
-        setCurrentDurationSec(e.duration);
-        setPlaytime(audioRecorderPlayer.mmssss(Math.floor(e.current_position)));
-        setDuration(audioRecorderPlayer.mmssss(Math.floor(e.duration)))
-    }
-    const onPausePlay = async(e)=>{
-        setIsPlaying(false)
-        await audioRecorderPlayer.pausePlayer()
-        .then(()=>{
-            console.log("Pause");
-        })
-        .catch(()=>{
-            console.log("couldn't pause");
-        })
-    }
-    const onStopPlay = async ()=>{
-        console.log('onStopPlay')
-        audioRecorderPlayer.stopPlayer()
-        audioRecorderPlayer.removePlayBackListener()
-    }
+    useEffect(() => {
+        return sound
+          ? () => {
+              console.log('Unloading Sound');
+              sound.unloadAsync(); }
+          : undefined;
+      }, [sound]);
 
     
     return(
@@ -109,7 +40,6 @@ const Recordings = ({navigation}) =>{
               
               <View style={styles.recording}>
                     <ScrollView>
-                        {/* recordings space */}
                     </ScrollView>
               </View>
               
@@ -135,7 +65,7 @@ const Recordings = ({navigation}) =>{
                             color='white'
                             size={40}     
                             callback= {()=>{
-                                isPlaying ? onPausePlay():onStartPlay()
+                                isPlaying ? console.log("jj"):playSound()
                             }}           
                         />
 
