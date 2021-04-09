21ed457c07b4b0b6c6c70fd5430cb226d0a948c5
Thomas Allen <gtallenpadi13@gmail.com>
2021-04-09 07:53:11 -0600

    Tratando de arreglar problemas con la creacion de audio en el contextp

--- src/providers/RecordingContext.js
+++ src/providers/RecordingContext.js
@@ -20,63 +20,87 @@ const recordingReducer = (state, action)=>{
                 ...state,
                 recordings: action.payload
             }
+        
         default:
             return state
     }
 }
 
 //https://dev.to/lankinen/expo-audio-upload-recording-to-firebase-storage-and-download-it-later-25o6
-const createRecording = (dispatch) = async (uri,fileName)=>{
+const createRecording = (dispatch) = (uri,fileName)=>{
+    aver(uri,fileName)
+}
+
+const aver = async (uri,fileName)=>{
     try
-    {
-        const blob = await new Promise((resolve,reject)=>{
-            const xhr = new XMLHttpRequest();
-            xhr.onload = ()=>{
-                try
-                {
-                    resolve(xhr.response)
-                }
-                catch(error)
-                {
-                    console.log(`error: ${error}`)
-                }
-            };
-            xhr.onerror = (e)=>{
-                console.log(e);
-                reject(new TypeError("Network request failed"))
-            };
-            xhr.responseType = "blob"
-            xhr.open("GET", uri,true)
-            xhr.send(null)
-        })
-        if (blob != null)
         {
-            const uriParts = uri.split(".")
-            const fileType = uriParts[uriParts.length - 1];
-            firebase
-            .storage()
-            .ref()
-            .child(`Audio/${fileName}.${fileType}`)
-            .put(blob,{
-                contentType: `audio/${fileType}`
+            const blob = await new Promise((resolve,reject)=>{
+                const xhr = new XMLHttpRequest();
+                xhr.onload = ()=>{
+                    try
+                    {
+                        resolve(xhr.response)
+                    }
+                    catch(error)
+                    {
+                        console.log(`error: ${error}`)
+                    }
+                };
+                xhr.onerror = (e)=>{
+                    console.log(e);
+                    reject(new TypeError("Network request failed"))
+                };
+                xhr.responseType = "blob"
+                xhr.open("GET", uri,true)
+                xhr.send(null)
             })
-            .then(()=>{
-                console.log('sent')
-            })
-            .catch((error)=>{
-                console.log(error);
-            })
+            if (blob != null)
+            {
+                
+                firebase
+                .storage()
+                .ref()
+                .child(`Audio/${fileName}`)
+                .put(blob,{
+                    contentType: `audio/${fileType}`
+                })
+                .then(()=>{
+                    console.log('sent')
+                
+                    const storageURL = `Audio/${fileName}`
+                    
+                    const recordingID = firebase.firestore().collection("Audios").doc().id
+                    firebase
+                    .firestore()
+                    .collection("Audios")
+                    .doc('GMewGC8wgvp5WyZoIc9j')
+                    .set({
+                        id: recordingID,
+                        fileName: `${fileName}`
+                    })
+                    .then(()=>{
+                        const recordingsRef = firebase.firestore().collection("Audios").doc(recordingID)
+                        recordingsRef.update({
+                            recordingPaths: firebase.firestore.FieldValue.arrayUnion(storageURL)
+                        })
+                    })
+                    
+                })
+                .catch((error)=>{
+                    console.log(error);
+                })
+            }
+            else
+            {
+                console.log('error with blob');
+            }
         }
-        else
-        {
-            console.log('error with blob');
-        }
-    }
     catch(error)
     {
         console.log(error)
     }
 }
+
 const getRecordings = (dispatch) = async () =>{
     const uri = await firebase
         .storage()
