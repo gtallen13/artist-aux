7f85737f270311202ae276e91d070a21b3f8bf1e
Thomas Allen <gtallenpadi13@gmail.com>
2021-04-09 09:28:05 -0600

    Incluyendo el contexto de proyectos

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useContext, useEffect } from 'react';
 import {Provider as AuthProvider} from './src/providers/AuthContext'
+import {Provider as ProjectProvider} from './src/providers/ProjectContext'
 import Navigation from './src/navigation/'
 export default function App() {
   
   return (
     <AuthProvider>
+      <ProjectProvider>
         <Navigation/>
+      </ProjectProvider>
     </AuthProvider>
   );
 }
