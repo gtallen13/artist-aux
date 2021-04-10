457486fc817aa04dd4b8f2018d2d5bb1842fc669
JonathanA03 <alemanjl.ix@gmail.com>
2021-04-10 15:11:47 -0600

    Implementacion de LongTimers para solucion de Timer en Android

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import React, { useContext, useEffect } from 'react';
 import {Provider as AuthProvider} from './src/providers/AuthContext'
 import {Provider as ProjectProvider} from './src/providers/ProjectContext'
 import Navigation from './src/navigation/'
+import LongTimers from './src/utils/LongTimer';
+
 export default function App() {
+  LongTimers();
   
   return (
     <AuthProvider>
