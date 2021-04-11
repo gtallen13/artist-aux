ee5be5d3aa7a617098fe858c728c198ef1b2c855
Thomas Allen <gtallenpadi13@gmail.com>
2021-04-10 18:51:43 -0600

    Nueva funcion para eliminar un audio

--- src/providers/RecordingContext.js
+++ src/providers/RecordingContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext'
+import {firebase} from '../firebase'
 
 const recordingReducer = (action, state)=>{
     switch(action.type)
@@ -13,21 +14,41 @@ const recordingReducer = (action, state)=>{
                 ...state,
                 currentRecording: action.payload
             }
+        case "deleteRecording":
+            return{
+                ...state,
+                recordingDeleted:true,
+            }
         default:
             return state
     }
 }
-
+const projectsRef = firebase.firestore().collection("Projects")
 const setCurrentRecording = (dispatch)=>(recording)=>{
     dispatch({type:"setCurrentRecording", payload:recording})
 }
-
+const deleteRecording=(dispatch)=>(id,recording)=>{
+    projectsRef
+    .doc(id)
+    .update({
+        recordings: firebase.firestore.FieldValue.arrayRemove(recording)
+    })
+    .then(()=>{
+        dispatch({type:"feedback", payload:"Recording Deleted"})
+        dispatch({type:"deleteRecording"})
+    })
+    .catch((error)=>{
+        dispatch({type:"feedback", payload:error.message})
+    })
+}
 export const {Provider, Context} = createDataContext(
     recordingReducer,
     {
-        setCurrentRecording
+        setCurrentRecording,
+        deleteRecording
     },
     {
-        currentRecording:{title:""}
+        currentRecording:{title:""},
+        recordingDeleted:false,
     }
 )
